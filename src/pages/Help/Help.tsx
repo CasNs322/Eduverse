9cad7375a7df08e7784a3127c1e54a623830fec9
CasNs322 <167998035+CasNs322@users.noreply.github.com>
2025-04-17 18:56:16 +0500

    end dev Help

--- src/pages/Help/Help.tsx
+++ src/pages/Help/Help.tsx
@@ -4,20 +4,23 @@ export default function Help() {
   return (
     <div className="Help">
       <div className="help">
-        <div className="title">ФОРМА ОБРАТНОЙ СВЯЗИ</div>
-        <div className="subtitle">Поля обязательные для заполнения</div>
-        <form action="" method="post" id="help">
+        <div className="title-help">ФОРМА ОБРАТНОЙ СВЯЗИ</div>
+        <div className="subtitle-help">Поля обязательные для заполнения</div>
+        <form action="" method="post" id="FormHelp">
           <input type="text" id="FIO" name="FIO" placeholder="ФИО" />
-          <input type="email" id="Email" name="Email" placeholder="Email" />
-          <input type="tel" id="number" name="number" placeholder="Телефон" />
-          <input
-            type="text"
-            id="TextError"
-            name="TextError"
-            placeholder="Сообщение"
-          />
-          <button type="reset">Сбросить</button>
-          <button type="submit">Отправить</button>
+          <div className="strok">
+            <input type="email" id="Email" name="Email" placeholder="Email" />
+            <input type="tel" id="Number" name="number" placeholder="Телефон" />
+          </div>
+          <textarea id="TextError" name="TextError" placeholder="Сообщение" />
+          <div className="button-help">
+            <button type="reset" className="reset-help">
+              Сбросить
+            </button>
+            <button type="submit" className="submit-help">
+              Отправить
+            </button>
+          </div>
         </form>
       </div>
     </div>
