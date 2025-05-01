1ca32b42989cce566a0ad443f53247a685dc6962
CasNs322 <167998035+CasNs322@users.noreply.github.com>
2025-05-01 20:51:28 +0500

    content

--- src/pages/Theme/Figma.tsx
+++ src/pages/Theme/Figma.tsx
@@ -5,34 +5,44 @@ import "./Figma.css";
 
 export default function Figma() {
   return (
-    <div className="right-menu">
-      <div className="name-courses">Web</div>
-      <div className="menu-chapter">Figma</div>
-      <div className="mappoint-themes">
-        <div className="mappoint">
-          <div className="point">
-            <img src={Point_in_progress} />
+    <div className="main">
+      <div className="right-menu">
+        <div className="name-courses">Web</div>
+        <div className="menu-chapter">Figma</div>
+        <div className="mappoint-themes">
+          <div className="mappoint">
+            <div className="point">
+              <img src={Point_in_progress} />
+            </div>
+            <div className="point">
+              <img src={Point_lock} />
+            </div>
+            <div className="point">
+              <img src={Point_lock} />
+            </div>
+            <div className="point">
+              <img src={Point_lock} />
+            </div>
+            <div className="point">
+              <img src={Endpoint_lock} />
+            </div>
           </div>
-          <div className="point">
-            <img src={Point_lock} />
-          </div>
-          <div className="point">
-            <img src={Point_lock} />
-          </div>
-          <div className="point">
-            <img src={Point_lock} />
-          </div>
-          <div className="point">
-            <img src={Endpoint_lock} />
+          <div className="themes">
+            <div className="theme">Знакомство с программой</div>
+            <div className="theme">Составление макета сайта</div>
+            <div className="theme">Составление макета сайта</div>
+            <div className="theme">Составление макета сайта</div>
+            <div className="theme">Итоговый кейс</div>
           </div>
         </div>
-        <div className="themes">
-          <div className="theme">Знакомство с программой</div>
-          <div className="theme">Составление макета сайта</div>
-          <div className="theme">Составление макета сайта</div>
-          <div className="theme">Составление макета сайта</div>
-          <div className="theme">Итоговый кейс</div>
-        </div>
+      </div>
+      <div className="content">
+        <div className="content_title"></div>
+        <div className="content_video"></div>
+        <div className="block1"></div>
+        <div className="block1"></div>
+        <div className="block1"></div>
+        <div className=""></div>
       </div>
     </div>
   );
