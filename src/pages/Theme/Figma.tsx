2c161e93669dd27212e8f2f175da98fdffa711e0
CasNs322 <167998035+CasNs322@users.noreply.github.com>
2025-05-02 20:21:55 +0500

    add content.json

--- src/pages/Theme/Figma.tsx
+++ src/pages/Theme/Figma.tsx
@@ -2,6 +2,7 @@ import Point_in_progress from "../../assets/points/point_in_progress.png";
 import Point_lock from "../../assets/points/point_lock.png";
 import Endpoint_lock from "../../assets/points/endpoint_lock.png";
 import "./Figma.css";
+import FigmaData from "../../content.json";
 
 export default function Figma() {
   return (
@@ -38,31 +39,21 @@ export default function Figma() {
       </div>
       <div className="content">
         <div className="content_title">
-          <div className="">Знакомство с программой</div>
+          <div className="">{FigmaData.lessons.figma.title}</div>
           <div className=""></div>
         </div>
         <div className="content_video">
           <iframe
-            width="901"
-            height="515"
-            src="https://www.youtube.com/embed/x9GYVG4EJYs"
+            width="854"
+            height="480"
+            src={FigmaData.lessons.figma.video}
             title="Основы Figma за 8 минут | Курс по работе с Фигма 2022"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           ></iframe>
         </div>
         <div className="block1">
           <div className="block1-text">
-            Фигма (Figma) — это облачный инструмент для дизайна интерфейсов и
-            прототипирования, который стал популярным среди UI/UX-дизайнеров
-            благодаря своей простоте, функциональности и возможностям для
-            совместной работы. Вот основные особенности Figma: Редактирование в
-            реальном времени: Несколько пользователей могут работать над одним
-            файлом одновременно, что идеально подходит для командной работы.
-            Можно оставлять комментарии и feedback прямо в проекте. Облачная
-            платформа: Все файлы хранятся в облаке, поэтому доступ к ним можно
-            получить с любого устройства с интернетом. Нет необходимости вручную
-            управлять версиями файлов. Кроссплатформенность: Работает на
-            Windows, macOS и Linux через браузер или десктопное приложение.
+            {FigmaData.lessons.figma.paragraf_1}
           </div>
           <div className="block1-img"></div>
         </div>
